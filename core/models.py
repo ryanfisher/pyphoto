80449dd7d79721f9e38654b3b2741c440db884f5
Ryan Fisher <ryancfisher@gmail.com>
2014-01-20 23:17:34 -0800

    Add core app for project utilities including the TimeStampedModel

---
+++ core/models.py
@@ -0,0 +1,13 @@
+# core/models.py
+from django.db import models
+
+class TimeStampedModel(models.Model):
+    """
+    An abstract base class model that provides self-updating
+    ``created`` and ``modified`` fields.
+    """
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
