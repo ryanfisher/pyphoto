ffb164e4e4ac284fb1c996fea5f556ce47ff2a73
Ryan Fisher <ryancfisher@gmail.com>
2014-01-22 22:17:21 -0800

    Add barebones login views

--- photo/urls.py
+++ photo/urls.py
@@ -10,3 +10,7 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += patterns('',
+    url(r'^login/$', 'profiles.views.login_view'),
+)
---
+++ profiles/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import redirect
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+
+def login_post(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        if user.is_active:
+            login(request, user)
+            return redirect('/')
+
+def login_view(request):
+    if not request.GET:
+        return HttpResponse('<html><body>Login Page!</body></html>')
+    elif request.POST:
+        return login_post(request)
