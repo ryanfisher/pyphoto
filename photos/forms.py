a415b29f4536bd9e87848bf844da036fdbaee4f6
Ryan Fisher <ryancfisher@gmail.com>
2014-02-06 21:43:38 -0800

    Remove commented out code

--- photos/forms.py
+++ photos/forms.py
@@ -2,9 +2,3 @@ from django import forms
 
 class ImageUploadForm(forms.Form):
     file = forms.FileField()
-
-    # def __init__(self, *args, **kwargs):
-    #     if request.FILES:
-    #         image_uploader = ImageUploader()
-    #         for file in request.FILES:
-    #             image_uploader.upload_image(file)
