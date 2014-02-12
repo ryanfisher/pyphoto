9ce63c88652d3703ac985d0b10c2ab7831bcd774
Ryan Fisher <ryancfisher@gmail.com>
2014-02-11 23:11:42 -0800

    Add photo info to database after uploading to s3

--- photos/views.py
+++ photos/views.py
@@ -5,6 +5,7 @@ from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
 
 from photos import forms
+from photos.models import Photo
 
 from boto.s3.connection import S3Connection
 from boto.s3.key import Key
@@ -18,6 +19,7 @@ class ImageUploader(object):
         k = Key(bucket)
         k.key = 'images/'+folder+'/'+file.name
         k.set_contents_from_file(file)
+        return '/' + k.key
 
 @login_required
 def upload(request):
@@ -31,5 +33,13 @@ def upload(request):
     elif request.method == 'POST':
         form = forms.ImageUploadForm(request.POST, request.FILES)
         if form.is_valid():
-            ImageUploader().upload_image(request.FILES['file'], request.user.username)
+            image_file = request.FILES['file']
+            path = ImageUploader().upload_image(image_file, request.user.username)
+            path = settings.AWS_IMAGE_BUCKET + path
+            Photo.objects.create(
+                url='//s3.amazonaws.com/' + path,
+                size=image_file.size,
+                iso=100,
+                user=request.user
+            )
         return redirect('/')
