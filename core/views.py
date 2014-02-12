84245ec53321bab9abdf8c1b52975014697f506a
Ryan Fisher <ryancfisher@gmail.com>
2014-02-11 23:18:10 -0800

    Add user photos to main photo feed

--- core/views.py
+++ core/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
 
+from photos.models import Photo
+
 @login_required
 def root(request):
-    return render_to_response('core/index.html')
+    photos = Photo.objects.filter(user=request.user)
+    return render_to_response('core/index.html', {'photos': photos})
