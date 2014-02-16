91fdc84fb4a6edf4709fbdf34358f8d368131368
Ryan Fisher <ryancfisher@gmail.com>
2014-02-15 22:30:29 -0800

    Move ImageUploader to services

---
+++ photos/services.py
@@ -0,0 +1,14 @@
+from boto.s3.connection import S3Connection
+from boto.s3.key import Key
+
+class ImageService(object):
+
+    def __init__(self):
+        conn = S3Connection(settings.AWS_ACCESS_KEY, settings.AWS_SECRET_KEY)
+        self.bucket = conn.get_bucket(settings.AWS_IMAGE_BUCKET, validate=False)
+
+    def upload_image(self, file, folder):
+        k = Key(self.bucket)
+        k.key = 'images/'+folder+'/'+file.name
+        k.set_contents_from_file(file)
+        return '/' + k.key
--- photos/views.py
+++ photos/views.py
@@ -4,38 +4,24 @@ from django.template import RequestContext
 
 from django.contrib.auth.decorators import login_required
 
-from photos import forms
+from photos.forms import ImageUploadForm
 from photos.models import Photo
-
-from boto.s3.connection import S3Connection
-from boto.s3.key import Key
-
-class ImageUploader(object):
-
-    def __init__(self):
-        conn = S3Connection(settings.AWS_ACCESS_KEY, settings.AWS_SECRET_KEY)
-        self.bucket = conn.get_bucket(settings.AWS_IMAGE_BUCKET, validate=False)
-
-    def upload_image(self, file, folder):
-        k = Key(self.bucket)
-        k.key = 'images/'+folder+'/'+file.name
-        k.set_contents_from_file(file)
-        return '/' + k.key
+from photos.services import ImageService
 
 @login_required
 def upload(request):
     if request.method == 'GET':
-        image_form = forms.ImageUploadForm()
+        image_form = ImageUploadForm()
         return render_to_response(
             'photos/upload.html',
             {'form': image_form},
             context_instance=RequestContext(request)
         )
     elif request.method == 'POST':
-        form = forms.ImageUploadForm(request.POST, request.FILES)
+        form = ImageUploadForm(request.POST, request.FILES)
         if form.is_valid():
             image_file = request.FILES['file']
-            path = ImageUploader().upload_image(image_file, request.user.username)
+            path = ImageService().upload_image(image_file, request.user.username)
             path = settings.AWS_IMAGE_BUCKET + path
             Photo.objects.create(
                 url='//s3.amazonaws.com/' + path,
