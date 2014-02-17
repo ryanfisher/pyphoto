f1050a2904d732fa132e03090a1b46eb8ed8e3db
Ryan Fisher <ryancfisher@gmail.com>
2014-02-16 22:08:39 -0800

    Remove unnecessary test setup methods

--- photos/tests/test_views.py
+++ photos/tests/test_views.py
@@ -2,13 +2,7 @@ from django.test import TestCase
 
 from photos import views
 
-class PhotosViewsTests(TestCase):
-
-    def setUp(self):
-        pass
-
-    def tearDown(self):
-        pass
+class PhotosViewsNotLoggedInTests(TestCase):
 
     def test_upload(self):
         response = self.client.get('/upload/')
