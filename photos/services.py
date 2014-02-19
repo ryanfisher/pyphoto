f9133e5166264001c17e114eb085b176085ba668
Ryan Fisher <ryancfisher@gmail.com>
2014-02-18 21:10:22 -0800

    Add method to read exif data in PhotoService

--- photos/services.py
+++ photos/services.py
@@ -2,15 +2,29 @@ from django.conf import settings
 
 from boto.s3.connection import S3Connection
 from boto.s3.key import Key
+from PIL import Image
+from PIL.ExifTags import TAGS
+
+import exifread
 
 class PhotoService(object):
 
-    def __init__(self):
+    def __init__(self, file):
+        self.file = file
         conn = S3Connection(settings.AWS_ACCESS_KEY, settings.AWS_SECRET_KEY)
         self.bucket = conn.get_bucket(settings.AWS_IMAGE_BUCKET, validate=False)
 
-    def upload_photo(self, file, folder):
+    def get_exif(self):
+        img = Image.open(self.file)
+        exifinfo = img._getexif()
+        exif_dict = {}
+        for tag, value in exifinfo.items():
+            decoded = TAGS.get(tag, tag)
+            exif_dict[decoded] = value
+        return exif_dict
+
+    def upload_photo(self, folder):
         k = Key(self.bucket)
-        k.key = 'images/'+folder+'/'+file.name
-        k.set_contents_from_file(file)
+        k.key = 'images/'+folder+'/'+self.file.name
+        k.set_contents_from_file(self.file)
         return '/' + k.key
--- photos/tests/test_services.py
+++ photos/tests/test_services.py
@@ -2,10 +2,24 @@ from django.test import TestCase
 
 from photos.services import PhotoService
 
+import os
+
 class PhotoServiceTest(TestCase):
 
     def setUp(self):
-        self.photo_service = PhotoService()
+        path = os.path.join(os.getcwd(), 'photos/tests/fixtures/blake-small.jpg')
+        self.maxDiff = None
+        self.photo_service = PhotoService(path)
 
     def test_instance(self):
         self.assertIsInstance(self.photo_service, PhotoService)
+
+    def test_get_exif(self):
+        result = self.photo_service.get_exif()
+        self.assertEqual(result['ExposureTime'], (1, 100))
+        self.assertEqual(result['FNumber'], (5, 1))
+        self.assertEqual(result['ISOSpeedRatings'], 800)
+        self.assertEqual(result['FocalLength'], (55, 1))
+        self.assertEqual(result['Make'], u'Canon')
+        self.assertEqual(result['Model'], u'Canon EOS REBEL T3')
+        self.assertEqual(result['LensModel'], u'EF-S15-85mm f/3.5-5.6 IS USM')
