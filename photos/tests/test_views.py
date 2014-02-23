c8de54c39beb090fd1bd358bebc41f0e993d10b8
Ryan Fisher <ryancfisher@gmail.com>
2014-02-22 18:16:08 -0800

    Fix test for photos show

--- photos/tests/test_views.py
+++ photos/tests/test_views.py
@@ -11,6 +11,14 @@ class PhotosViewsNotLoggedInTests(TestCase):
 
 class PhotosViewsTests(TestCase):
 
+    def setUp(self):
+        photo = Photo.objects.create(
+            url="photos/photo.jpg",
+            size=23423,
+            user_id=1
+        )
+
     def test_show(self):
-        response = self.client.get('photos/99')
-        self.assertEquals(response.status_code, 404)
+        response = self.client.get('/photos/1')
+        self.assertEquals(response.status_code, 200)
+        self.assertContains(response, 'photos/photo.jpg')
