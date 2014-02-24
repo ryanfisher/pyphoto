dabecdfec3710bbe35b72ccf0d0bc86f9cf7f2d5
Ryan Fisher <ryancfisher@gmail.com>
2014-02-23 22:28:54 -0800

    Add coverage test command that generates coverage report

---
+++ photos/management/commands/coveragetest.py
@@ -0,0 +1,13 @@
+from django.core.management.base import BaseCommand, CommandError
+
+import subprocess, os
+
+class Command(BaseCommand):
+    help = "Runs test code and creates coverage report"
+
+    def handle(self, *args, **options):
+        result = subprocess.call("coverage run manage.py test --settings=project.settings.test", shell=True)
+        if result == 0:
+            subprocess.call('coverage html --include="$SITE_URL*" --omit="admin.py"', shell=True)
+        else:
+            self.stdout.write("TEST FAILURES")
