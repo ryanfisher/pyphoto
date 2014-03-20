024c20e05ee4015f9671e9040fa831fa7cd0f450
Ryan Fisher <ryancfisher@gmail.com>
2014-03-19 19:54:20 -0700

    Get ajax file upload to work

--- project/static/javascript/application.js
+++ project/static/javascript/application.js
@@ -2,15 +2,40 @@ var Uploader = Backbone.View.extend({
   el: "#uploader",
 
   events: {
-    'click input[type="submit"]': "upload_photo",
+    'submit form': "upload_photo",
   },
 
   initialize: function () {
     console.log("Uploader initialized")
+    this.uploading = false
   },
 
   upload_photo: function () {
     event.preventDefault();
+    if (this.uploading) {
+      console.log("currently uploading");
+      return;
+    };
+    this.uploading = true;
+    form = $(event.target);
+    file_input = form.find('#id_file');
+    photo_file = file_input[0].files[0];
+    form_data = new FormData();
+    form_data.append('file', photo_file)
+    csrf_token = $('input[name="csrfmiddlewaretoken"]').val()
+    _this = this;
+    $.ajax({
+      type: "POST",
+      url: "/upload",
+      data: form_data,
+      headers: { 'X-CSRFToken': csrf_token },
+      success: function (data) {
+        _this.uploading = false;
+        alert('Done Uploading');
+      },
+      processData: false,
+      contentType: false
+    });
   }
 });
 
