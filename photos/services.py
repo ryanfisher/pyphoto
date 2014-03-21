d0518dfa901f9d897147911a6111ad84a42490a9
Ryan Fisher <ryancfisher@gmail.com>
2014-03-20 22:14:27 -0700

    Install flake8 and change service.py to remove warnings

--- photos/services.py
+++ photos/services.py
@@ -1,7 +1,6 @@
 from django.conf import settings
 
 from boto.s3.connection import S3Connection
-from boto.s3.key import Key
 from PIL import Image
 from PIL.ExifTags import TAGS
 
@@ -11,11 +10,13 @@ from photos.models import Photo
 
 THUMBNAIL_SIZE = 600
 
+
 class TemporaryImageFile(object):
     def __init__(self, uploaded_file):
         random_folder = 'random'
         dirs_path = 'tmp/'+random_folder+'/'
-        if not os.path.exists(dirs_path): os.makedirs(dirs_path)
+        if not os.path.exists(dirs_path):
+            os.makedirs(dirs_path)
         self.path = dirs_path + uploaded_file.name
         with open(self.path, 'wb+') as destination:
             for chunk in uploaded_file.chunks():
@@ -24,27 +25,38 @@ class TemporaryImageFile(object):
     def delete(self):
         os.remove(self.path)
 
+
 class PhotoService(object):
 
     def __init__(self, uploaded_file, user):
         self.uploaded_file = uploaded_file
         self.user = user
         conn = S3Connection(settings.AWS_ACCESS_KEY, settings.AWS_SECRET_KEY)
-        self.bucket = conn.get_bucket(settings.AWS_IMAGE_BUCKET, validate=False)
+        self.bucket = conn.get_bucket(
+            settings.AWS_IMAGE_BUCKET,
+            validate=False
+        )
 
     def send_to_s3(self, file, file_prefix=""):
-        k = self.bucket.new_key('images/'+self.user.username[0]+'/'+self.user.username[1:]+'/'+file_prefix+self.uploaded_file.name)
+        hashed_dirs = self.user.username[0]+'/'+self.user.username[1:]+'/'
+        file_key = 'images/'+hashed_dirs+file_prefix+self.uploaded_file.name
+        k = self.bucket.new_key(file_key)
         k.set_contents_from_file(file, replace=False)
         return settings.AWS_IMAGE_BUCKET + '/' + k.key
 
     def send_string_to_s3(self, string_file, file_prefix="thumbnail_"):
-        k = self.bucket.new_key('images/'+self.user.username[0]+'/'+self.user.username[1:]+'/'+file_prefix+self.uploaded_file.name)
-        k.set_contents_from_string(string_file, headers={"Content-Type": "image/jpeg"})
+        hashed_dirs = self.user.username[0]+'/'+self.user.username[1:]+'/'
+        file_key = 'images/'+hashed_dirs+file_prefix+self.uploaded_file.name
+        k = self.bucket.new_key(file_key)
+        k.set_contents_from_string(
+            string_file,
+            headers={"Content-Type": "image/jpeg"}
+        )
         return settings.AWS_IMAGE_BUCKET + '/' + k.key
 
     def create_and_store_thumbnail(self, img):
         width = THUMBNAIL_SIZE * img.size[0] / img.size[0]
-        img.thumbnail((width,THUMBNAIL_SIZE), Image.ANTIALIAS)
+        img.thumbnail((width, THUMBNAIL_SIZE), Image.ANTIALIAS)
 
         from cStringIO import StringIO
         image_string = StringIO()
