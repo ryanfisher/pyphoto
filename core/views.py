448de04f0cb700c61181dcc210aafacbb7d37853
Ryan Fisher <ryancfisher@gmail.com>
2014-05-31 20:11:59 -0700

    Use bootstrapped photos on home page

--- core/views.py
+++ core/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
 
+from photos.serializers import PhotoSerializer
 from photos.models import Photo
+
+import json
+
 
 @login_required
 def root(request):
     photos = Photo.objects.filter(user=request.user)
+    serializer = PhotoSerializer(photos, many=True)
+    photos = json.dumps(serializer.data)
     return render_to_response('core/index.html', {'photos': photos})
