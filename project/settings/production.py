dc3c7fdf1bb6c371dfbdc43b3a0ba12ecdeaac8b
Ryan Fisher <ryancfisher@gmail.com>
2014-07-18 12:10:20 -0700

    Change production database to postresql

--- project/settings/production.py
+++ project/settings/production.py
@@ -8,7 +8,7 @@ TEMPLATE_DEBUG = DEBUG
 
 DATABASES = {
   "default": {
-    "ENGINE": "django.db.backends.mysql",
+    "ENGINE": "django.db.backends.postgresql_psycopg2",
     "NAME": os.environ['RDS_DB_NAME'],
     "USER": os.environ['RDS_USERNAME'],
     "PASSWORD": os.environ['RDS_PASSWORD'],
